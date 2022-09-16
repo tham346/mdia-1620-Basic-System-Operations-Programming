b592ec7cc7378640d22e58f6a67b4f2fa0b7b906
Thamasha Amarasekara <thamashaamarasekara@Thamashas-MacBook-Pro.local>
2022-09-16 14:45:34 -0700

    new code

---
+++ Part1/test.js
@@ -0,0 +1,10 @@
+// Declare
+// Declare a variable called electronic devices 
+// camel case?
+var electronicDevices; 
+
+// pascal case?
+var ElectronicDevices; 
+
+// snake case?
+var Electronic_Devices;
